1f29b8b7ef8fd6a962c5280756e857e37dbd8e8b
sithanga rashmika <87372439+IT21041716@users.noreply.github.com>
2023-04-30 17:19:07 +0530

    back end

---
+++ Backend/CommissionServer/comModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const ComSchema = new Schema({
+
+    Com_ID: {
+        type:String,
+        require:true
+    },
+    Order_ID: {
+        type: String,
+        required: true
+    },
+    Total_Amount: {
+        type: String,
+        required: true,
+    },
+    Commission: {
+        type: String,
+        required: true,
+    },
+})
+export default mongoose.model("Commission",ComSchema )
